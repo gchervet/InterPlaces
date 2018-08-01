34f99603ec1459e5a548fdae6c7b556d60f605dc
German Chervet <31969834+gchervet@users.noreply.github.com>
2018-08-01 17:09:49 -0300

    First commit

---
+++ js/mapCodeBehind.js
@@ -0,0 +1,36 @@
+var mymap = L.map('mapid').setView([-34.6037, -58.3816], 13);
+
+L.tileLayer('https://api.tiles.mapbox.com/v4/{id}/{z}/{x}/{y}.png?access_token=pk.eyJ1IjoibWFwYm94IiwiYSI6ImNpejY4NXVycTA2emYycXBndHRqcmZ3N3gifQ.rJcFIG214AriISLbB6B5aw', {
+    maxZoom: 18,
+    attribution: 'Map data &copy; <a href="https://www.openstreetmap.org/">OpenStreetMap</a> contributors, ' +
+        '<a href="https://creativecommons.org/licenses/by-sa/2.0/">CC-BY-SA</a>, ' +
+        'Imagery © <a href="https://www.mapbox.com/">Mapbox</a>',
+    id: 'mapbox.streets'
+}).addTo(mymap);
+
+var placesArray = 
+[
+    { 
+        coordenates: [-34.6037, -58.3816],
+        name: 'example1',
+        tel: 12398713
+    }
+];
+
+for(var i in placesArray){
+    var actualPlace = placesArray[i];
+    L.marker(actualPlace.coordenates).addTo(mymap)
+    .bindPopup("<b>example1</b><br /> Teléfono: " + actualPlace.tel);
+}
+
+var popup = L.popup();
+
+function onMapClick(e) {
+    popup
+        .setLatLng(e.latlng)
+        .setContent("You clicked the map at " + e.latlng.toString())
+        .openOn(mymap);
+}
+
+mymap.on('click', onMapClick);
+
